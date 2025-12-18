047105a5eb23810b6ad7e15e785a5122f03bce86
The-Sagebrush-Sage <Wermut-Kraft@yandex.ru>
2025-12-18 18:56:24 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,90 @@
+// База данных пользователей (в реальном проекте это должно быть на сервере)
+const userDatabase = {
+    "user_5sjr50": {
+        username: "user_5sjr50",
+        password: "123456",
+        profileName: "Дизайнер Профи",
+        profileNickname: "_pro",
+        profileStyle: "Дизайн",
+        avatar: "https://i.postimg.cc/K81gkwBL/photo-5226705699080367487-x.jpg",
+        socialLinks: [
+            { platform: "telegram", username: "_pro" },
+            { platform: "instagram", username: "pro" }
+        ],
+        works: [
+            "https://i.postimg.cc/DwQtzWBf/photo-5244699232638725577-y.jpg",
+            "https://i.postimg.cc/Pf9GVnCH/photo-5244699232638725578-y.jpg",
+            "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg",
+            "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg",
+            "https://i.postimg.cc/Vk3Hs0tN/photo-5271895704291372799-c.jpg"
+        ],
+        publishedOn: "design",
+        encryptedPassword: "1234567",
+        profileDescription: "Профессиональный ",
+        registrationDate: "2025-12-07T17:49:22.018Z"
+    },
+
+    "user_abc123": {
+        username: "user_abc123",
+        profileName: "Гусь",
+        profileNickname: "@artist_test",
+        profileStyle: "Иллюстрация",
+        avatar: "https://i.postimg.cc/6qvPXv17/photo-5305270117032127745-y.jpg",
+        socialLinks: [
+            { platform: "telegram", username: "artist_test" }
+        ],
+        works: [
+            "https://i.postimg.cc/XYcTq9Nd/photo-5305270117032127729-y.jpg",
+            "https://i.postimg.cc/SKSHMjj5/photo-5305270117032127730-y.jpg"
+        ],
+        publishedOn: "artist",
+        userPassword: "123456",
+        profileDescription: "Иллюстратор и концепт-художник",
+
+    },
+
+    "user_prem789": {
+        username: "user_prem789",
+        profileName: "Prem Художник",
+        profileNickname: "@prem_artist",
+        profileStyle: "Prem Artist",
+        avatar: "https://i.postimg.cc/K81gkwBL/photo-5226705699080367487-x.jpg",
+        socialLinks: [
+            { platform: "telegram", username: "prem_artist" },
+            { platform: "artstation", username: "prem_artist" }
+        ],
+        works: [
+            "https://i.postimg.cc/DwQtzWBf/photo-5244699232638725577-y.jpg",
+            "https://i.postimg.cc/Pf9GVnCH/photo-5244699232638725578-y.jpg",
+            "https://i.postimg.cc/NfH3WCXH/photo-5244699232638725579-y.jpg"
+        ],
+        publishedOn: "prem_artist",
+        userPassword: "prem888",
+        profileDescription: "Премиум художник с эксклюзивными работами",
+
+    },
+
+    "user__21e3": {
+        username: "user__21e3",
+        profileName: "Еее",
+        profileNickname: "А",
+        profileStyle: "Графический дизайн",
+        avatar: "https://i.postimg.cc/K81gkwBL/photo-5226705699080367487-x.jpg",
+        socialLinks: [
+            { platform: "telegram", username: "А" }
+        ],
+        works: [
+            "https://i.postimg.cc/K81gkwBL/photo-5226705699080367487-x.jpg",
+            "https://i.postimg.cc/K81gkwBL/photo-5226705699080367487-x.jpg",
+            "https://i.postimg.cc/K81gkwBL/photo-5226705699080367487-x.jpg",
+            "https://i.postimg.cc/K81gkwBL/photo-5226705699080367487-x.jpg",
+            "https://i.postimg.cc/K81gkwBL/photo-5226705699080367487-x.jpg"
+        ],
+        publishedOn: "design",
+        userPassword: "123456",
+        profileDescription: "",
+
+    },
+
+
+};
